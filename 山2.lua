b56f4912501ffb87a0137d4abfe9620d6149c157
qazwsx123wer <rangwochangchang@qq.com>
2025-08-19 08:20:45 +0800

    Update 山2.lua

--- 山2.lua
+++ 山2.lua
@@ -132,12 +132,16 @@ end)
 
 local tab2 = DrRayLibrary.newTab("脚本汉化版游戏", "ImageIdLogoHere")
 
-tab2.newButton("被遗弃", " ", function()
-    loadstring(game:HttpGet"https://raw.githubusercontent.com/OWOWOWWOW/HTT/main/HT.lua")()
+tab2.newButton("", " ", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/qazwsx123wer/owo/refs/heads/main/%E5%B1%B1%E8%84%9A%E6%9C%AC.lua"))()
+end)
+
+tab2.newButton("", " ", function()
+    print('Hello!')
 end)
 
 tab2.newButton("云脚本测试版", " ", function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/XiaoYunCN/6666666666/main/%E4%BA%91%E8%84%9A%E6%9C%AC%E6%B5%8B%E8%AF%95%E7%89%88%E4%BA%91%E8%84%9A%E6%9C%AC%E6%B5%8B%E8%AF%95%E7%89%88Xiao%20Yun.lua", true))()
+    print('Hello!')
 end)
 
 tab2.newButton("即将推出", "即将推出", function()
